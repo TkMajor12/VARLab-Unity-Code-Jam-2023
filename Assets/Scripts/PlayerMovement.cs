0bc001ceaa0076de5c61e1233408f85665024587
tehtko <moderndsnsam@gmail.com>
2023-02-28 17:45:21 -0500

    Fix merge

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement Values")]
-<<<<<<< HEAD
-    [SerializeField] private float moveSpeed = 30f;
-
-=======
     [SerializeField] protected float moveSpeed = 15f;
     [SerializeField] protected float jumpForce = 5f;
     [SerializeField] protected float gravityScale = 1f;
 
     [Header("Inputs")]
->>>>>>> ed39dfb61b1daa8c9fd45558627feca68dfc55f8
     [SerializeField] protected string HorizontalInput = "Horizontal";
     [SerializeField] protected string VerticalInput = "Vertical";
     [SerializeField] protected string JumpInput = "Jump";
@@ -23,13 +18,8 @@ public class PlayerMovement : MonoBehaviour
     protected new Rigidbody rigidbody;
     protected new CapsuleCollider collider;
 
-<<<<<<< HEAD
-    // Awake is called before the first frame update
-    void Awake()
-=======
     // Start is called before the first frame update
     private void Awake()
->>>>>>> ed39dfb61b1daa8c9fd45558627feca68dfc55f8
     {
         rigidbody = GetComponent<Rigidbody>();
         collider = GetComponent<CapsuleCollider>();
@@ -38,22 +28,6 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-<<<<<<< HEAD
-    void Update()
-    {
-        var movementVector = CalculateMovementVector();
-        HandleMovement(movementVector);
-    }
-
-    protected virtual Vector3 CalculateMovementVector()
-    {
-        Jump();
-
-        Vector3 movementVector = new Vector3(
-            Input.GetAxisRaw(HorizontalInput) * -1,
-            Input.GetAxisRaw(JumpInput),
-            Input.GetAxisRaw(VerticalInput) * -1);
-=======
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -79,45 +53,20 @@ public class PlayerMovement : MonoBehaviour
 
         movementVector += transform.right * Input.GetAxisRaw(HorizontalInput) * -1;
         movementVector += transform.forward * Input.GetAxisRaw(VerticalInput) * -1;
->>>>>>> ed39dfb61b1daa8c9fd45558627feca68dfc55f8
 
         // Going forward/back and left/right at the same time creates a right triangle with magnitude sqrt(2).
         // Normalizing this makes you move at the same speed regardless of input combination.
         movementVector = movementVector.normalized;
 
-<<<<<<< HEAD
-        // Multiply our movement vector by our set move speed before returning it
-        return movementVector * moveSpeed;
-    }
-
-    /// <summary> Updates the element's velocity </summary>
-=======
         return movementVector *= moveSpeed; ;
     }
 
->>>>>>> ed39dfb61b1daa8c9fd45558627feca68dfc55f8
     protected virtual void HandleMovement(Vector3 velocity)
     {
         // Y-axis velocity is always preserved
         velocity.y = rigidbody.velocity.y;
         rigidbody.velocity = velocity;
     }
-<<<<<<< HEAD
-
-    protected virtual void Jump()
-    {
-        Vector3 up = new Vector3(0, 1000, 0);
-        Vector3 down = new Vector3(0, 100, 0);
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("JUMP");
-            rigidbody.AddForce(up * 50, ForceMode.Force);
-        }
-
-        rigidbody.AddForce(down * (-5), ForceMode.Force);
-    }
-}
-=======
     
     protected virtual void Jump()
     {
@@ -125,4 +74,3 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log("Jumping");
     }
 }
->>>>>>> ed39dfb61b1daa8c9fd45558627feca68dfc55f8
