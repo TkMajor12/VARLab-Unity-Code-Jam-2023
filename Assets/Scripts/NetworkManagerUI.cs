a31c34950bbb52fbadb8e4246a817fffd5b48161
TkMajor12 <ethanmajor9@gmail.com>
2023-03-03 03:03:32 -0500

    lemme sleep

--- Assets/Scripts/NetworkManagerUI.cs
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -1,6 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,15 +18,31 @@ public class NetworkManagerUI : MonoBehaviour
     [SerializeField] private Button clientBtn;
 
     // Start is called before the first frame update
-    void Awake()
+    private void Awake()
     {
         serverBtn.onClick.AddListener(() => 
         {
             NetworkManager.Singleton.StartServer();
         });
-        hostBtn.onClick.AddListener(() => 
+        hostBtn.onClick.AddListener(async () => 
         {
-            NetworkManager.Singleton.StartHost();
+            try
+            {
+                Allocation allocation = await RelayService.Instance.CreateAllocationAsync(3);
+
+                string JoinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+                Debug.Log(JoinCode);
+
+                RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
+
+                NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+
+                NetworkManager.Singleton.StartHost();
+            }
+            catch (RelayServiceException e)
+            {
+                Debug.Log(e.ToString());
+            }
         });
         clientBtn.onClick.AddListener(() => 
         {
